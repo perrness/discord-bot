103cfd6d38265096e143978467f478a939d8f80a
Per Næss <perness3009@gmail.com>
2023-02-26 18:38:35 +0100

    feat: add google cloud logging

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-const dotenv = require('dotenv');
-dotenv.config();
-
-// Require the necessary discord.js classes
 const { Client, Events, GatewayIntentBits } = require('discord.js');
+const winston = require('winston');
+const { LoggingWinston } = require('@google-cloud/logging-winston');
+const dotenv = require('dotenv');
+
+dotenv.config();
 const token = process.env.DISCORD_TOKEN;
 const GENERAL_CHAT_ID = process.env.GENERAL_CHAT_ID;
 const MUTED_CHAT_ID= process.env.MUTED_CHAT_ID;
+
+const loggingWinston = new LoggingWinston();
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  defaultMeta: { service: 'jeff-bot' },
+  transports: [
+    new winston.transports.Console(),
+    loggingWinston
+  ],
+});
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildVoiceStates] });
@@ -13,7 +25,7 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBit
 // When the client is ready, run this code (only once)
 // We use 'c' for the event parameter to keep it separate from the already defined 'client'
 client.once(Events.ClientReady, c => {
-	console.log(`Ready! Logged in as ${c.user.tag}`);
+  logger.info(`Ready! Logged in as ${c.user.tag}`);
 });
 //
 // Log in to Discord with your client's token
@@ -22,7 +34,7 @@ client.login(token);
 client.on('ready', async () => {
   const channel = await client.channels.fetch(GENERAL_CHAT_ID);
   const message = await channel.messages.fetch({limit: 1});
-  console.log(message.first().content);
+  logger.info(message.first().content);
 });
 
 client.on('voiceStateUpdate', async (oldVoiceState, newVoiceState) => {
